92604d6c2ed4b8644219f183e4d64eb184b73846
John L. Preston <wcerfgba@riseup.net>
2018-03-30 13:14:26 +0100

    So baeutifel

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const async = require('async');
 const glob = require('glob');
+const EmbedJS = require('embed-js');
+const url = require('embed-plugin-url');
+const emoji = require('embed-plugin-emoji');
+const github = require('embed-plugin-github');
 
 const main = () => {
   async.waterfall([
@@ -35,41 +39,74 @@ const sortFilesByName = (files, cb) => async.sortBy(
   (err, result) => cb(err, result.reverse())
 );
 
-const layoutHtml = (files, cb) => cb(null, `
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: monospace;
-  max-width: 32rem;
-}
-</style>
-</head>
-<body>
-${each(files, file => formatFile(`
+const layoutHtml = (files, cb) => {
+  async.map(
+    files,
+    (file, cb) => formatFile(`
 # ${file.filename}
 
 ${file.data}
 
-`))}
+`,
+    cb),
+    (err, parts) => cb(err, `
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://unpkg.com/emoji.css/dist/emoji.min.css">
+<style>
+html {
+  font-size: 15px;
+  line-height: 1.12;
+  font-family: monospace;
+  max-width: 36rem;
+  white-space: pre-wrap;
+  background: #f00;
+  color: #0ff;
+  font-weight: bold;
+}
+a, a:link, a:visited, a:hover, a:active {
+  color: #0ff;
+}
+.ec {
+  font-size: 1.1rem;
+}
+.ec::before {
+  hue-rotate(120deg); /* hue hue */
+}
+</style>
+</head>
+<body>
+${parts.join('')}
 </body>
-</html>
-`);
+</html>   
+`
+    )
+  )
+}
 
-const each = (xs, f) => xs.map(f).join('');
+const formatFile = (data, cb) => {
+  embed(data, cb)
+ /* async.map(
+    data.split('\n'),
+    embed,
+    (err, lines) => cb(err, lines.join('\n'))
+  ) */
+}
 
-const formatFile = (data) => (
-  data
-    .split('\n')
-    .map(line => linkify(line))
-    .join('<br/>\n')
-);
-
-const linkify = (str) => (
-  str.replace(/https?:\/\/\S+/g, '<a href="$&">$&</a>')
-);
+const embed = (str, cb) => {
+  if (!str) { return cb(null, ''); }
+  const embedder = new EmbedJS({
+    input: str,
+    plugins: [
+      url(), emoji()
+    ]
+  });
+  embedder.text()
+    .then(({ result }) => cb(null, result))
+    .catch(err => cb(err, null))
+}
 
 const writeIndex = (data, cb) => fs.writeFile(
   'index.html',
